df3c0b88b6886e15043faa69eefd909ebdd1f8e0
Human <onemarket1world@gmail.com>
2025-02-15 07:17:23 +0200

    Create astro.config.mjs

---
+++ PWA/astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import { injectManifest } from 'workbox-build';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://ONEW0RLD2030.github.io/humanverse.github.io/',
+  adapter: '@astrojs/netlify',
+  vite: {
+    plugins: [{
+      name: 'pwa',
+      async closeBundle() {
+        await injectManifest({
+          swSrc: 'public/sw.js',
+          swDest: 'dist/sw.js',
+          globDirectory: 'dist',
+          globPatterns: ['**/*.{html,js,css,json}'],
+        });
+      }
+    }]
+  }
+});
